60b5667caa9af6b4f3ae5ef11860526084d21d5d
tduong5 <timmypduong@gmail.com>
2022-06-13 18:13:40 -0700

    Ft/load env (#4)
    * deploy workflow: add checkout code step
    
    * load env variable before running db migration

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -e # exit if return non-zero
 
 echo "run db migration"
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up # similar one found in Makefile
 
 echo "start the app"
